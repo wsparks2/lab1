e84da1ea190a4cf0c7953952fc45e2aa772c641b
Student <student@lab-311-030.cs.csubak.edu>
2020-01-24 09:50:06 -0800

    Friday code updates

--- lab1.cpp
+++ lab1.cpp
@@ -40,7 +40,7 @@ using namespace std;
 #include <X11/keysym.h>
 #include <GL/glx.h>
 
-const int MAX_PARTICLES = 8;
+const int MAX_PARTICLES = 4000;
 const float GRAVITY     = 0.1;
 
 //some structures
@@ -221,7 +221,7 @@ void makeParticle(int x, int y)
 	p->s.center.x = x;
 	p->s.center.y = y;
 	//p->velocity.y = -4.0;
-	p->velocity.x =  1.0;
+	p->velocity.x =  ((float)rand()/ (float)RAND_MAX) + 0.5;
     p->velocity.y = ((float)rand()/ (float)RAND_MAX) + 0.5;
     // set x velocity to random here too
 	++g.n;
@@ -247,8 +247,9 @@ void check_mouse(XEvent *e)
 		if (e->xbutton.button==1) {
 			//Left button was pressed.
 			int y = g.yres - e->xbutton.y;
-			makeParticle(e->xbutton.x, y);
-            makeParticle(e->xbutton.x, y);
+			for (int i = 0; i < 20; i++){ 
+			    makeParticle(e->xbutton.x, y);
+			}
 			return;
 		}
 		if (e->xbutton.button==3) {
@@ -262,7 +263,10 @@ void check_mouse(XEvent *e)
 			savex = e->xbutton.x;
 			savey = e->xbutton.y;
 			//Code placed here will execute whenever the mouse moves.
-
+			int y = g.yres - e->xbutton.y;
+			for (int i = 0; i < 20; i++){ 
+			    makeParticle(e->xbutton.x, y);
+			}
 
 		}
 	}
@@ -296,12 +300,18 @@ void movement()
     for (int i = 0; i < g.n; i++) {
 	Particle *p = &g.particle[i];
 	p->s.center.x += p->velocity.x;
-    p->velocity.y = p->velocity.y - GRAVITY;
+    	p->velocity.y = p->velocity.y - GRAVITY;
 	p->s.center.y += p->velocity.y;
 
 	//check for collision with shapes...
-	//Shape *s;
-
+	Shape *s = &g.box;
+	if (p->s.center.y < s->center.y + s->height && 
+		p->s.center.x > s->center.x - s->width &&
+		p->s.center.x < s->center.x + s->width &&
+	       	p->s.center.y > s->center.y - s->height  ){
+	    p->s.center.y = s->center.y + s->height;
+	    p->velocity.y = p->velocity.y *-.5;
+	}
 
 
 
